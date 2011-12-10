52247f77df2f4e8640652f80cae07fa122a2300e
Owen Wang <wangfowen@gmail.com>
2011-12-10 12:03:34 -0500

    set up rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialalarmclock::Application.config.secret_token = '3ad9d45f9f915df5fed505728714b58693dc3fc251e0ffe04a3e07979299c1d722e329df474d1795a1b19291a17b23469f76d55e0f114fa974800333293a3d51'
